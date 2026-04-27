ca47e7e6ee0ced0b05c15d0ad02adb10ec71acd3
Tea Kidder <teakidder@gmail.com>
2026-04-27 00:28:50 -0700

    ye

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,7 +2,7 @@ export default function Home() {
 
 
     return (
-        <div className="flex flex-wrap p-40 pt-20 gap-10 h-screen">
+        <div className="flex flex-wrap p-40 pt-20 gap-10 h-210">
                 <div className=" flex-2 min-w-80 flex-col border-2 border-white bg-[url('worm.jpg')] bg-cover bg-center">
                     <p>About me</p>
                 </div>
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -8,7 +8,7 @@ export default function Root() {
         <div className="relative min-h-screen text-white">
             <div className="fixed inset-0 z-0">
             </div>
-            <nav className="relative z-10 w-full border-b-2 border-white bg-[url('worm.jpg')] bg-cover bg-size-100%-20%">
+            <nav className="relative z-10 w-full border-b-2 border-white bg-[url('worm.jpg')] bg-cover bg-size-100%-20% fixed">
                 <ul className="flex items-center justify-around px-3 py-5 text-3xl">
                     
                     <li>
